68c5e1e95e7df4243e45906d824a6ec986c988fb
kaine0261-max <kaine0261@gmail.com>
2025-11-03 23:02:14 +0800

    Add storage link to start.sh for Railway image loading

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Simple start script without migrations
+# Create storage symlink for images
+php artisan storage:link
+
+# Start the Laravel server
 php artisan serve --host=0.0.0.0 --port=$PORT
