331ecbc1f398886d204c8608494e7f2278a31d1b
kaine0261-max <kaine0261@gmail.com>
2025-11-04 11:03:17 +0800

    Revert all Cloudinary changes - restore to working state

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,14 +60,6 @@ return [
             'report' => false,
         ],
 
-        'cloudinary' => [
-            'driver' => 'cloudinary',
-            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-            'api_key' => env('CLOUDINARY_API_KEY'),
-            'api_secret' => env('CLOUDINARY_API_SECRET'),
-            'url' => env('CLOUDINARY_URL'),
-        ],
-
     ],
 
     /*
